5b41fc1d82da99548064bbbc45ed0721c2aa1f18
vVviewVv <viewissara1246@gmail.com>
2021-01-12 14:00:47 +0700

    ...

--- app_template.js
+++ app_template.js
@@ -1,1 +1,21 @@
 //your code here!
+function Cal(){
+    
+    var num = document.getElementById("input-number").value;
+    //var ans = document.getElementById("ans").value;
+    document.getElementById("input-number").value = "";
+    
+    if (num === '') {
+        alert("You must write something!");
+    } else {
+        var cal = 620610805-num;
+        ans = parseInt(cal);
+        alert(cal);
+    }
+}
+
+function Create(){
+    var body = document.getElementsByTagName( "body" )[0];
+    var tbl = document.createElement( "table" );
+    var tblBody = document.createElement( "tbody" );
+}
